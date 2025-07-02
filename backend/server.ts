46e0f329b60d58b69cfd6bab8a3cf3a9fde71af9
JustinDodemaide <103222511+JustinDodemaide@users.noreply.github.com>
2025-07-02 00:05:33 -0700

    Type definitions, optimization algorithm start
    The map() function was having problems with the undefined typing from the placeholder menu and users, so they were changed to interfaces.

--- backend/server.ts
+++ backend/server.ts
@@ -3,12 +3,50 @@ import express, { Request, Response } from 'express';
 const app = express();
 const PORT = 3000;
 
-const sampleMenu = [
-  {id: 1, name: "Burger", price: 12.99, calories: 600},
-  {id: 2, name: "Sushi", price: 15.99, calories: 400},
-  {id: 3, name: "Crunchwrap", price: 6.19, calories: 530},
-]
+// --- Definitions ---
+interface MenuItem {
+  id: number;
+  name: string;
+  price: number;
+  calories: number;
+  score: number;
+}
 
+interface User {
+  scores: UserScores;
+}
+
+interface UserScores {
+  [key: number]: number;
+}
+
+// --- Placeholder values ---
+const defaultScore = -1;
+
+const sampleMenu: MenuItem[] = [
+  {id: 1, name: "Burger", price: 12.99, calories: 600, score: defaultScore},
+  {id: 2, name: "Sushi", price: 15.99, calories: 400, score: defaultScore},
+  {id: 3, name: "Crunchwrap", price: 6.19, calories: 530, score: defaultScore},
+];
+
+const users: { [key: number]: User } = {
+  1:{
+    scores:{
+        1: 5,
+        2: 3,
+        3: 4
+    }
+  },
+  2:{
+    scores:{
+      1: 1,
+      2: 3,
+      3: 4
+    }
+  }
+};
+
+// --- Endpoints ---
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Restaurant Optimizer API' });
 });
@@ -21,8 +59,48 @@ app.get('/menu', (req: Request, res: Response) => {
   res.json(sampleMenu);
 });
 
-app.get('/optimize/:budget', (req: Request, res: Response) => {
-  const budget = parseFloat(req.params.budget);
-  const affordableItems = sampleMenu[0];
-  res.json(affordableItems);
+app.get('/optimize/:userId/:budget', (req: Request, res: Response) => {
+  const userId = parseInt(req.params.userId, 10);
+  const budgetNum = parseFloat(req.params.budget);
+
+  //if (budgetNum > 2500) {
+    //return res.status(400).json({ error: 'Budget is too high' });
+  //}
+
+  // This is the line that was unhappy with undefined types
+  let userScores:{[key: number]: number} = {};
+  if (users[userId]) {
+    userScores = users[userId].scores;
+  }
+
+  // Add the user's ratings to the items
+  let scoredItems = [];
+  for (const item of sampleMenu) {
+    const itemCopy = {...item}
+    const userScore = userScores[item.id];
+    if(userScore !== undefined){
+      itemCopy.score = userScore;
+    }
+    scoredItems.push(itemCopy);
+  }
+
+  // TODO: Unbounded knapsack implementation
+
+  /*
+  array m[0..n, 0..W];
+  for j from 0 to W do:
+      m[0, j] := 0
+  for i from 1 to n do:
+      m[i, 0] := 0
+
+  for i from 1 to n do:
+      for j from 1 to W do:
+          if w[i] > j then:
+              m[i, j] := m[i-1, j]
+          else:
+              m[i, j] := max(m[i-1, j], m[i-1, j-w[i]] + v[i])
+  */
+
+  
+  res.send("")
 });
